ae7b31d5332d281b72254b1b05e48473182f48cf
coaltail <coaltail420@gmail.com>
2025-04-26 21:12:14 +0200

    dodani requirements.txt, azurirani server.py sa cert.pem i key.pem

--- server.py
+++ server.py
@@ -1,41 +1,53 @@
-from flask import Flask, request 
-from flask_socketio import SocketIO, emit
-from flask import Flask, render_template
+from flask import Flask, request, render_template, abort, send_from_directory
+from flask_socketio import SocketIO
+import ssl
+import os
 
 app = Flask(__name__)
 socketio = SocketIO(app)
 
 players = {}
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/lobby')
-def lobby():
-    return render_template('lobby.html')
+@app.route("/")
+@app.route("/<page>")
+def render_page(page="index"):
+    try:
+        return render_template(f"{page}.html")
+    except:  # noqa: E722
+        abort(404)  # if template not found
 
-@app.route('/game')
-def game():
-    return render_template('game.html')
 
-@socketio.on('join')
+@app.route("/ar/")
+@app.route("/ar/<path:filename>")
+def serve_static(filename="index.html"):
+    file_path = os.path.join("ar", filename)
+    if not os.path.isfile(file_path):
+        abort(404)  
+
+    return send_from_directory("ar", filename)
+
+
+@socketio.on("join")
 def handle_join(data):
-    player_name = data['name']
+    player_name = data["name"]
     players[request.sid] = player_name
-    socketio.emit('players_update', players)
+    socketio.emit("players_update", players)
 
-@socketio.on('disconnect')
+
+@socketio.on("disconnect")
 def handle_disconnect():
     if request.sid in players:
         del players[request.sid]
-    socketio.emit('players_update', players)
+    socketio.emit("players_update", players)
 
-@socketio.on('start_game')
+
+@socketio.on("start_game")
 def start_game():
-    socketio.emit('game_started')
-
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.emit("game_started")
 
 
+if __name__ == "__main__":
+    context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+    context.load_cert_chain(certfile="cert.pem", keyfile="key.pem")
+    socketio.run(app, host="0.0.0.0", port=5000, debug=True, ssl_context=context)
